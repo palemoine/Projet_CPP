9c0c50d966b74a68c0b6b0453004450e1a071e33
PA <pa@pa.(none)>
2013-03-21 09:25:37 +0100

    Modifie monde.cpp

--- src/Monde.cpp
+++ src/Monde.cpp
@@ -16,37 +16,37 @@ Monde::Monde() {
 }
 
 
-//void Monde::afficher()const
-//{
-//	cout<<"Vector : "<<endl;
-//	for (unsigned i=0 ; i< size() ; i++)
-//		at(i)->afficher();
-//	cout<<"Map : "<<endl;
-//	map<PositionHex, unsigned int>::const_iterator Iter;
-//	for(Iter=getMap().begin(); Iter!=getMap().end() ; Iter++)
-//		cout<<Iter->first<<" "<<Iter->second<<endl;
-//}
+void Monde::afficher()const
+{
+	cout<<"Vector : "<<endl;
+	for (unsigned i=0 ; i< size() ; i++)
+		at(i)->afficherElement();
+	cout<<"Map : "<<endl;
+	map<PositionHex, unsigned int>::const_iterator Iter;
+	for(Iter=getMap().begin(); Iter!=getMap().end() ; Iter++)
+		cout<<Iter->first<<" "<<Iter->second<<endl;
+}
 
-//unsigned int
-//Monde::estValide(Position _pos) const
-//{
-//  unsigned int r = 0;
-//  map<Position, unsigned int>::const_iterator it;
-//  it = carte.end();
-//  if (carte.find(_pos) != it)
-//    r = 1;
-//  return r;
-//}
-//
-//void
-//Monde::ajouter(Element * _elt)
-//{
-//  if (estValide(_elt->getPos()))
-//  {
-//    push_back(_elt); // ajoute un élément dans le vecteur
-//    carte.insert(pair<Position, unsigned> (_elt->getPos(), size()-1)); // size() car Monde hérite de Vecteur
-//  }
-//}
+unsigned int
+Monde::estValide(Position _pos) const
+{
+  unsigned int r = 0;
+  map<Position, unsigned int>::const_iterator it;
+  it = laMap.end();
+  if (laMap.find(_pos) != it)
+    r = 1;
+  return r;
+}
+
+void
+Monde::ajouter(Element * _elt)
+{
+  if (estValide(_elt->getPosition()))
+  {
+    push_back(_elt); // Ajout -elt dans le vecteur
+    laMap.insert(pair<Position, unsigned> (_elt->getPosition(), size()-1));
+  }
+}
 
 
 
